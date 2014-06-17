6e0af000b108f77edcd656576e12b33ec8a60544
eddyystop <johnsz@gmail.com>
2014-06-17 13:01:23 -0400

    Adding readme to Solder & making prop private

--- mixins/Solder/Solder.js
+++ mixins/Solder/Solder.js
@@ -1,16 +1,17 @@
 // Solder ======================================================================
 function Solder () {
   if (!(this instanceof Solder)) { return new Solder(); }
-  this.constructors = {};
+  this._constructors = {};
 }
 
 Solder.prototype = {
-  setMixin: function (name, constructor) { this.constructors[name] = constructor; },
+  setMixin: function (name, constructor) { this._constructors[name] = constructor; },
 
   injectMixins: function (names, ctrl) {
     if (typeof names === 'string') { names = [names]; }
     for (var i = 0, len = names.length; i < len; i += 1) {
-      ctrl[names[i]] = new this.constructors[names[i]](ctrl);
+      var ctrlName = ctrl[names[i]] = new this._constructors[names[i]](ctrl);
+      if (ctrlName._onInjection) ctrlName._onInjection();
     }
   }
 };
