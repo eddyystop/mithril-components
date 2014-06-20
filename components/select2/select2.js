4a7e9b79250e8b8b9e9f147bf3d75234111fbd9a
eddyystop <johnsz@gmail.com>
2014-06-20 19:33:58 -0400

    Adding param docs for readme's for select, select2, table, tabs

--- components/select2/select2.js
+++ components/select2/select2.js
@@ -1,17 +1,8 @@
 /*global m, $ */
 
 // Select ======================================================================
-// copied for convenience from http://lhorie.github.io/mithril/integration.html
-/** @namespace */
 var select2 = {};
 
-/**
- select2 config factory. The params in this doc refer to properties of the `ctrl` argument
- @param {Object} data - the data with which to populate the <option> list
- @param {number} value - the id of the item in `data` that we want to select
- @param {function(Object id)} onchange - the event handler to call when the selection changes.
- `id` is the the same as `value`
- */
 select2.config = function(ctrl) {
   return function(element, isInitialized) {
     var el = $(element);
