3ad398f2a60f0f8d3c6ced7992f574c28eed1981
Satyam <satyam@satyam.com.ar>
2014-06-23 14:25:23 +0200

    Allow for other actions besides routing
    Tabs can now be configured to respond and act in other ways than
    routing.
    The tabs configuration has been moved to the view,
    since it mostly deals with presentation matters.
    The controller part of the tabs deals with responding to changes in
    tabs, either originating internally or from the UI.

--- components/tabs/tabs.js
+++ components/tabs/tabs.js
@@ -4,63 +4,89 @@
 var mc = mc || {};
 
 mc.Tabs = {
-  controller: function (tabs, activeTab) {
-    this.tabs = tabs || [];
-    this.activeTab = activeTab || '';
-  },
+	controller: function (tabSelector, action) {
+		this.tabSelector = tabSelector || function () {
+			return m.route.param('tab');
+		};
+		this.action = action;
+	},
 
-  view: function (ctrl, selectors, attrs, overrides) {
-    selectors = selectors || {};
-    attrs = attrs || {};
-    overrides = overrides || {};
+	view: function (ctrl, tabs, selectors, attrs) {
+		selectors = selectors || {};
+		attrs = attrs || {};
 
-    var tabs = normalizeTabs(overrides.tabs || ctrl.tabs, ctrl),
-      activeTab = overrides.activeTab || ctrl.activeTab ||
-        m.route.param('tab') || Object.keys(tabs)[0];
+		var tabSelector = ctrl.tabSelector,
+			activeTab = (typeof tabSelector == 'function' ? tabSelector() : tabSelector) || Object.keys(tabs)[0];
+		tabs = normalizeTabs(tabs, ctrl);
 
-    return [
-      m( 'div' + (selectors._parent || ''), attrs._parent || {},
-        m('ul', Object.keys(tabs).map(tab))
-      ),
-      tabs[activeTab].view(tabs[activeTab].ctrl)
-    ];
+		return [
+			m('div' + (selectors._parent || ''), attrs._parent || {},
+				m('ul', Object.keys(tabs).map(tab))
+			),
+			tabs[activeTab].view(tabs[activeTab].ctrl)
+			];
 
-    function tab (name) {
-      var selected = activeTab === name,
-        selector = (selected && selectors._activeAnchor ?
-          selectors._activeAnchor : selectors._anchor) || '',
-        attrParam = (selected && attrs._activeAnchor ?
-          attrs._activeAnchor : attrs._anchor) || '',
-        attr = {};
+		function tab(name) {
+			var selected = activeTab === name,
+				selector = (selected && selectors._activeAnchor ?
+					selectors._activeAnchor : selectors._anchor) || '',
+				attrParam = (selected && attrs._activeAnchor ?
+					attrs._activeAnchor : attrs._anchor) || '',
+				attr = {};
 
-      if (attrParam) { merge(attr, attrParam); }
-      merge(attr, { href: '/' + name, config: m.route });
+			if (attrParam) {
+				merge(attr, attrParam);
+			}
+			merge(attr, {
+				href: '/' + name,
+				config: !ctrl.action && m.route
+			});
 
-      return m('li' + (selectors._item || ''), attrs._item || {},
-        m('a' + selector, attr, tabs[name].label || '')
-      );
-    }
+			if (ctrl.action) {
+				attr.onclick = function (e) {
+					if (e.ctrlKey || e.metaKey || e.which == 2) return;
+					e.preventDefault();
+					ctrl.action(e.currentTarget.getAttribute('href').substr(1), e);
+				};
+			}
 
-    function normalizeTabs (tabs, ctrl) {
-      var norm = {},
-        lastCtrl = ctrl || {};
+			return m('li' + (selectors._item || ''), attrs._item || {},
+				m('a' + selector, attr, tabs[name].label || '')
+			);
+		}
 
-      Object.keys(tabs).forEach(function (key) { // depends on .keys() returning keys in order defined
-        var tab = tabs[key];
-        if (typeof tab === 'function') {
-          norm[key] = { view: tab, ctrl: lastCtrl, label: key };
-        } else {
-          if (tab.ctrl) { lastCtrl = tab.ctrl; }
-          norm[key] = { view: tab.view, ctrl: lastCtrl, label: tab.label || key};
-        }
-      });
+		function normalizeTabs(tabs, ctrl) {
+			var norm = {},
+				lastCtrl = ctrl || {};
 
-      return norm;
-    }
+			Object.keys(tabs).forEach(function (key) { // depends on .keys() returning keys in order defined
+				var tab = tabs[key];
+				if (typeof tab === 'function') {
+					norm[key] = {
+						view: tab,
+						ctrl: lastCtrl,
+						label: key
+					};
+				} else {
+					if (tab.ctrl) {
+						lastCtrl = tab.ctrl;
+					}
+					norm[key] = {
+						view: tab.view,
+						ctrl: lastCtrl,
+						label: tab.label || key
+					};
+				}
+			});
+
+			return norm;
+		}
 
 
-    function merge (to, from) {
-      for (var key in from) { to[key] = from[key]; }  // jshint ignore:line
-    }
-  }
+		function merge(to, from) {
+			for (var key in from) {
+				to[key] = from[key];
+			}
+		}
+	}
 };
--- public/js/tabs-runner.js
+++ public/js/tabs-runner.js
@@ -1,73 +1,111 @@
-/* global m:false */
+/* jshint browser:true */
+/* global m:false,mc:false */
 
 var app = {
-  name: m.prop('John'),
-  dept: m.prop('dev'),
-  list: m.prop([
-    { "author": "Albert Camus",
-      "quote": "Those who lack the courage will always find a philosophy to justify it."
+	name: m.prop('John'),
+	dept: m.prop('dev'),
+	list: m.prop([
+		{
+			"author": "Albert Camus",
+			"quote": "Those who lack the courage will always find a philosophy to justify it."
     },
-    { "author": "Plato",
-      "quote": "You can discover more about a person in an hour of play than in a year of conversation."
+		{
+			"author": "Plato",
+			"quote": "You can discover more about a person in an hour of play than in a year of conversation."
     },
-    { "author": "Ludwig Wittgenstein",
-      "quote": "If people never did silly things nothing intelligent would ever get done."
+		{
+			"author": "Ludwig Wittgenstein",
+			"quote": "If people never did silly things nothing intelligent would ever get done."
     }
   ]),
 
-  controller: function () {
-    var tabs = {
-      list: { view: app.listView, ctrl: this, legend: 'theList' },
-      settings: { view: app.settingsView },
-      about: app.aboutView
-    };
+	controller: function () {
 
-    this.name = m.prop(app.name());
-    this.dept = m.prop(app.dept());
-    this.list = m.prop(app.list());
+		this.name = m.prop(app.name());
+		this.dept = m.prop(app.dept());
+		this.list = m.prop(app.list());
 
-    this.tabs = new mc.Tabs.controller(tabs);
-  },
+		this.tabsCtrl = new mc.Tabs.controller();
+		
+		this.activeTab = m.prop('settings');
+		this.tabsCtrl1 = new mc.Tabs.controller(this.activeTab, function () {
+			console.log(arguments);
+		});
+		
+		var t = 0, self = this;
+		setInterval(function () {
+			m.startComputation();
+			t = (t + 1) % 3;
+			self.activeTab(['list','settings','about'][t]);
+			m.endComputation();
+		}, 2000);
+	},
 
-  view: function (ctrl) {
-    var selectors = { _parent: '.tabs', _activeAnchor: '.selected' };
-    return mc.Tabs.view(ctrl.tabs, selectors);
-  },
+	view: function (ctrl) {
+		var tabs = {
+			list: {
+				view: app.listView,
+				ctrl: this,
+				label: 'theList'
+			},
+			settings: {
+				view: app.settingsView
+			},
+			about: app.aboutView
+		};
 
-  listView: function (ctrl) {
-    return m('ul.itemlist',
-      ctrl.list().map(function (item) {
-        return m("li", item.quote + ' - ' + item.author);
-      })
-    );
-  },
-  settingsView: function (ctrl) {
-    return m('form', [ // <form> is optional
+		var selectors = {
+			_parent: '.tabs',
+			_activeAnchor: '.selected'
+		};
+		return [
+			mc.Tabs.view(ctrl.tabsCtrl, tabs, selectors),
+			mc.Tabs.view(ctrl.tabsCtrl1, tabs, selectors)
+		];
+	},
+
+	listView: function (ctrl) {
+		return m('ul.itemlist',
+			ctrl.list().map(function (item) {
+				return m("li", item.quote + ' - ' + item.author);
+			})
+		);
+	},
+	settingsView: function (ctrl) {
+		return m('form', [ // <form> is optional
       m('div', 'name (4 to 10 chars)'),
-      m('input',
-        { value: ctrl.name(),
-          onchange: m.withAttr('value', app.name) }),
+      m('input', {
+				value: ctrl.name(),
+				onchange: m.withAttr('value', app.name)
+			}),
       m('div', 'department'),
-      m('input',
-        { value: ctrl.dept(), onchange: m.withAttr('value', app.dept) })
+      m('input', {
+				value: ctrl.dept(),
+				onchange: m.withAttr('value', app.dept)
+			})
     ]);
-  },
-  aboutView: function (ctrl) {
-    return m(
-      ".about", [
+	},
+	aboutView: function (ctrl) {
+		return m(
+			".about", [
         "Add a quote in JSON",
         m("hr"),
-        m("textarea", { rows: 10, cols: 80,
-            onchange: function () { app.list(JSON.parse(this.value)); } },
-          JSON.stringify(ctrl.list())
+        m("textarea", {
+						rows: 10,
+						cols: 80,
+						onchange: function () {
+							app.list(JSON.parse(this.value));
+						}
+					},
+					JSON.stringify(ctrl.list())
         ),
         m('p', 'If you go to the [list] tab, you will see the changes at once.')
       ]
-    );
-  }
+		);
+	}
 };
 
 m.route(document.body, '/', {
-  '/': app,
-  '/:tab': app
+	'/': app,
+	'/:tab': app
 });
